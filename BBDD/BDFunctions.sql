623692960ad4853e33d1996c44c8fe04e81e14b6
andres02-art <a.arregoces35@gmail.com>
2023-01-28 17:09:14 -0500

    alphaBD132.1
    -se ha añadido modulo para el funcionamiento de la BD

---
+++ BBDD/BDFunctions.sql
@@ -0,0 +1,28 @@
+Use mypokebd;
+
+/*
+funicionamiento dinamico de la base de datos al ser editada
+*/
+
+Delimiter ;;
+
+/*
+permite crear un security para cada usuario
+*/
+create or replace definer=root@localhost trigger _SecurityModule after insert on users
+for each row
+begin
+    insert into _security(no)
+    value(new.ID_User) on duplicate key update no=new.ID_User;
+end;;
+
+/*
+Permite asignar un poquemon a una view
+*/
+
+create or replace definer=root@localhost trigger _ViewPokemon after insert on pokemon
+for each row
+begin
+    insert into views()
+end;;
+Delimiter ;
