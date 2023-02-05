525fc713eec1cc56d11bee427ee64731f6277de3
andres02-art <a.arregoces35@gmail.com>
2023-02-05 11:26:26 -0500

    Version 0.3.1
    -se ha implementado correctamente la funcion de iniciar sesion al servidor
    -se ha corregido un error que impedia crear y conectar al usuario

--- Api/Php/logUser.php
+++ Api/Php/logUser.php
@@ -6,16 +6,50 @@ if (!isset($_SESSION)) {
 }
 include('./Security.php');
 include('./Functions.php');
+try {
 
-print_r($_POST);
+    print_r($_POST);
+    switch ($_POST['Activity']) {
+        case 'Register':
+            $noUser = GenKey(15, true, 'users', 'no');
+            $noPerson = GenKey(15, true, 'person', 'ID_Person');
+            $PersonSentence = "INSERT into person(ID_Person, name, lastName, __credentials) values('$noPerson', '$_POST[name]', '$_POST[lastname]', '$_POST[__credentials]')";
+            $UserSentence = "INSERT into users(no, PrsID_Person, nickName, _registerDate, __address, _password) values('$noUser', '$noPerson', '$_POST[nickName]', CURRENT_DATE, '$_POST[__addressreg]', SHA2('$_POST[_passwordreg]', '512'))";
+            $ConnectSentence = "CREATE or replace user '$_POST[nickName]'@'%' identified by '$_POST[_passwordreg]'";
+            $GrantConnect = "GRANT ALL privileges on mypokebd.* to '$_POST[nickName]'@'%'";
+            mysqli_multi_query(ReStartConnect(), $PersonSentence);
+            mysqli_multi_query(ReStartConnect(), $UserSentence);
+            mysqli_multi_query(ReStartConnect(), $ConnectSentence);
+            mysqli_multi_query(SecurityAccessDataBase('mysql'), $GrantConnect);
+            mysqli_multi_query(SecurityAccessDataBase('mysql'), "Flush privileges");
+            break;
+        case 'LogIn':
+            $log = SetFetchQuery("SELECT * from users inner join person where users.PrsID_Person = person.ID_Person AND users._password = SHA2('$_POST[_password]', '512')", "SessionFetch")->fetch_assoc();
+            echo $log['nickName'];
+            $LogConnect = mysqli_query(SecurityAccessDataBase("mysql"), "SELECT * from user where User = '$log[nickName]'")->fetch_assoc();
+            echo $LogConnect['Password'];
+            $_SESSION['LogIn'] = true;
+            $_SESSION['MasterUser']['User'] = $LogConnect;
+            $_SESSION['MasterUser']['MasterConnect'] = "$log[nickName]";
+            $_SESSION['MasterUser']['MasterPassword'] = "$_POST[_password]";
+            $_SESSION['MasterUser']['MasterService'] = "$_SERVER[HTTP_HOST]";
+            break;
 
-$noUser = GenKey(15, true, 'users', 'no');
-$noPerson = GenKey(15, true, 'person', 'ID_Person');
-$PersonSentence = "INSERT into person(ID_Person, name, lastName, __credentials) values('$noPerson', '$_POST[name]', '$_POST[lastname]', '$_POST[__credentials]')on duplicate key update ID_Person = '$noPerson'";
-$UserSentence = "INSERT into users(no, PrsID_Person, nickName, _registerDate, __address, _password) values('$noUser', '$noPerson', '$_POST[nickName]', CURRENT_DATE, '$_POST[__addressreg]', '$_POST[_passwordreg]')on duplicate key update ID_User = '$noUser$noPerson'";
-echo $UserSentence;
-mysqli_multi_query(ReStartConnect(), $PersonSentence);
-mysqli_multi_query(ReStartConnect(), $UserSentence);
+        default:
+            echo 'Actividad No encontrada';
+            break;
+    }
+} catch (\Exception $ex) {
+    switch ($ex->getCode()) {
+        case '1062':
+            //ResponseProcess(['Error', "Usuario Existente, intentelo nuevamente", $ex->getCode()]);
+            break;
 
-ResponseProcess(['Succes', 'Usuario creado correctamente', 3])
+        default:
+            //ResponseProcess(['Warming', "ha ocurrido: " . $ex->getMessage(), $ex->getCode()]);
+            echo $ex->getMessage();
+            echo $ex->getCode();
+            break;
+    }
+}
 ?>
